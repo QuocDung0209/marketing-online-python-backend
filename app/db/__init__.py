9c88b97152b5a67b16167d592fc2ad17f51832a8
KenjiNguyen0209 <kenjisunnycloudy@gmail.com>
2021-05-31 17:53:12 +0700

    [Migrations] Configure Alembic for migrations

---
+++ app/db/__init__.py
@@ -0,0 +1,4 @@
+# Import all the models, so that the Base class
+# has them before being imported by Alembic.
+
+from .base import Base  # noqa
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 from app import __version__
+from typing import Any
 
 
-def test_version():
+def test_version() -> Any:
     assert __version__ == '0.1.0'
