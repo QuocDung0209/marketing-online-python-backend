bc2f8c8acfe472dfd3feca2badeb2b7abb698b6d
QuocDung0209 <quocdung02091997@gmail.com>
2021-06-02 20:36:38 +0700

    [Users Feature] Add update user by user id endpoint

--- app/api/api_v1/users/endpoints.py
+++ app/api/api_v1/users/endpoints.py
@@ -91,3 +91,24 @@ def read_user_by_id(
     if not crud.user.is_superuser(current_user):
         raise HTTPException(status_code=400, detail=NOT_ENOUGH_PRIVILEGES)
     return user
+
+
+@router.put("/{user_id}", response_model=schemas.User)
+def update_user_by_user_id(
+    *,
+    db: Session = Depends(depends.get_db),
+    user_id: int,
+    user_in: schemas.UserUpdate,
+    current_user: models.User = Depends(depends.get_current_active_superuser),
+) -> Any:
+    """
+    Update a user.
+    """
+    user = crud.user.get(db, id=user_id)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="The user with this username does not exist in the system",
+        )
+    user = crud.user.update(db, db_obj=user, obj_in=user_in)
+    return user
