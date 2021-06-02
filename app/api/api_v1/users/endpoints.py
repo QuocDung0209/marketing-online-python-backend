30c50523f9579fa8392dd87243d554b1e90ec007
QuocDung0209 <quocdung02091997@gmail.com>
2021-06-02 20:33:16 +0700

    [Users Feature] Add update user me endpoint

--- app/api/api_v1/users/endpoints.py
+++ app/api/api_v1/users/endpoints.py
@@ -39,3 +39,27 @@ async def create_user(
     Create new user.
     """
     return await create_user_service(db, user_in)
+
+
+@router.put("/me", response_model=schemas.User)
+def update_user_me(
+    *,
+    db: Session = Depends(depends.get_db),
+    password: str = Body(None),
+    full_name: str = Body(None),
+    email: EmailStr = Body(None),
+    current_user: models.User = Depends(depends.get_current_active_user),
+) -> Any:
+    """
+    Update own user.
+    """
+    current_user_data = jsonable_encoder(current_user)
+    user_in = schemas.UserUpdate(**current_user_data)
+    if password is not None:
+        user_in.password = password
+    if full_name is not None:
+        user_in.full_name = full_name
+    if email is not None:
+        user_in.email = email
+    user = crud.user.update(db, db_obj=current_user, obj_in=user_in)
+    return user
